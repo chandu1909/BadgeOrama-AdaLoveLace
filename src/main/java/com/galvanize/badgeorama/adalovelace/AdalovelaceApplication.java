94a7066dd85a7fba041f57d5022916b7f2ecd0c9
chandu1909 <chandra.polavarapu516@topper.wku.edu>
2018-11-27 13:47:56 -0700

    Initial commit

---
+++ src/main/java/com/galvanize/badgeorama/adalovelace/AdalovelaceApplication.java
@@ -0,0 +1,12 @@
+package com.galvanize.badgeorama.adalovelace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdalovelaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdalovelaceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/galvanize/badgeorama/adalovelace/AdalovelaceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.galvanize.badgeorama.adalovelace;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdalovelaceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
